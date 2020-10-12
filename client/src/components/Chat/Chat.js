0ad36c03b2bcc76ec84a6fa9a7d8f584e6ce6b11
sushkr34 <sendmailtosushant@gmail.com>
2020-10-12 10:38:20 +0530

    chat application

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,8 +1,56 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
+import querySting from 'query-string';
+import io from 'socket.io-client';
 
-const  Chat =()=>{
+import './Chat.css'
+import InfoBar from '../InfoBar/InfoBar';
+import Input from '../Input/Input';
+import Messages from '../Messages/Messages';
+
+let socket;
+
+const  Chat =({location})=>{
+    const [name,setName]=useState('');
+    const [room,setRoom]=useState('');
+    const [messages,setMessages]=useState([]);
+    const [message,setMessage]=useState('');
+    const ENDPOINT='localhost:5000';
+    useEffect(()=>{
+        const {name,room}=querySting.parse(location.search);
+        socket =io(ENDPOINT);
+        setName(name);
+        setRoom(room);
+        socket.emit('join',{name,room},()=>{
+           
+        });
+        return ()=>{
+            socket.emit('disconnect');
+
+            socket.off();
+        }
+    },[ENDPOINT,location.search]);
+    useEffect(()=>{
+        socket.on('message',(message)=>{
+            setMessages([...messages,message])
+        })
+    },[messages])
+
+    const sendMessage=(e)=>{
+        e.preventDefault();
+        if(message){
+            socket.emit('sendMessage',message,()=>setMessage(''));
+        }
+    }
     return(
-        <h1>Chat</h1>
+        <div className="outerContainer">
+            <div className="container">
+                <InfoBar  room={room}/>
+                <Messages  messages={messages}name={name}/>
+                <Input message={message}setMessage={setMessage} sendMessage={sendMessage} />
+                
+            </div>
+            <TextContainer users={users}/>
+        </div>
     )
 }
 
